56d46fc423642dd12c06102fa6377352d77e6ba0
Max Dayton <daytonmaximus@gmail.com>
2022-11-26 22:25:14 -0800

    path params example

--- src/main.rs
+++ src/main.rs
@@ -24,6 +24,13 @@ async fn cool_fact() -> String {
     return String::from("Ferris is a Rustacean");
 }
 
+#[get("/users/{name}")]
+async fn get_user_by_name(path: web::Path<(String,)>) -> HttpResponse {
+    let user_name: String = path.into_inner().0;
+    println!("get_user_by_name name: {}", user_name);
+    HttpResponse::Ok().body(format!("User info: {}", user_name))
+}
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
@@ -37,6 +44,7 @@ async fn main() -> std::io::Result<()> {
             .service(web::scope("/app").route("/fact", web::get().to(cool_fact)))
             .service(index)
             .service(echo)
+            .service(get_user_by_name)
             .route("/hey", web::get().to(manual_hello))
     })
     .bind(("127.0.0.1", 8080))?
